2854feaea8ed32c447d016e2028328740263e23a
Roman Levinzon <levinzon.roman@gmail.com>
2021-10-20 15:46:24 +0200

    refactor: move deps declarations to the buildSrc

--- accompanist-navigation/build.gradle.kts
+++ accompanist-navigation/build.gradle.kts
@@ -51,12 +51,12 @@ mavenPublish {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.6.0")
-    implementation("androidx.navigation:navigation-compose:2.4.0-alpha10")
-    implementation("androidx.compose.ui:ui:${rootProject.extra["compose_version"]}")
+    implementation("androidx.navigation:navigation-compose:${Deps.composeNavigation}")
+    implementation("androidx.compose.ui:ui:${Deps.compose}")
     api((project(":core")))
 
-    implementation("com.google.accompanist:accompanist-navigation-animation:0.20.0")
-    implementation("com.google.accompanist:accompanist-navigation-material:0.20.0")
+    implementation("com.google.accompanist:accompanist-navigation-animation:${Deps.accompanist}")
+    implementation("com.google.accompanist:accompanist-navigation-material:${Deps.accompanist}")
 
 
     testImplementation("junit:junit:4.+")
