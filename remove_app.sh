63a9a15de7ce8b8265efe60240f28796732801bd
Roman Levinzon <levinzon.roman@gmail.com>
2021-12-05 16:41:19 +0100

    ci: remove app module before publish

--- remove_app.sh
+++ remove_app.sh
@@ -1,4 +1,4 @@
-dependencyResolutionManagement {
+echo "dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
@@ -7,8 +7,9 @@ dependencyResolutionManagement {
         jcenter() // Warning: this repository is going to shut down soon
     }
 }
-rootProject.name = "router"
-include(":annotations")
-include(":compiler")
-include(":core")
-include(":accompanist-navigation")
+rootProject.name = \"router\"
+include(\":annotations\")
+include(\":compiler\")
+include(\":core\")
+include(\":accompanist-navigation\")
+"  > settings.gradle.kts
