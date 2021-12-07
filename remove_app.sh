1a7dde6108e6aef663c9e433c531074a3edb9fa2
Roman Levinzon <levinzon.roman@gmail.com>
2021-12-07 13:02:08 +0100

    ci: add remove_app.sh to exclude sample app from building

---
+++ remove_app.sh
@@ -0,0 +1,15 @@
+echo "dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        mavenLocal()
+        jcenter() // Warning: this repository is going to shut down soon
+    }
+}
+rootProject.name = \"router\"
+include(\":annotations\")
+include(\":compiler\")
+include(\":core\")
+include(\":accompanist-navigation\")
+"  > settings.gradle.kts
