7d61a853916c310713444bfc1274830db1e0049a
Roman Levinzon <levinzon.roman@gmail.com>
2022-08-14 18:34:16 +0200

    feat: add codegen to generate the internal scope implementation

--- codegen/src/main/java/com/levinzonr/saferoute/codegen/codegen/NavGraphScopesCodegen.kt
+++ codegen/src/main/java/com/levinzonr/saferoute/codegen/codegen/NavGraphScopesCodegen.kt
@@ -1,21 +1,30 @@
 package com.levinzonr.saferoute.codegen.codegen
 
 import com.levinzonr.saferoute.codegen.codegen.extensions.ComposableFunction
+import com.levinzonr.saferoute.codegen.constants.ClassNames
 import com.levinzonr.saferoute.codegen.core.FilesGen
 import com.levinzonr.saferoute.codegen.core.GeneratorUnit
 import com.levinzonr.saferoute.codegen.models.ModelData
 import com.levinzonr.saferoute.codegen.models.NavGraphData
 import com.levinzonr.saferoute.codegen.models.RouteData
+import com.squareup.kotlinpoet.ClassName
+import com.squareup.kotlinpoet.CodeBlock
 import com.squareup.kotlinpoet.FileSpec
 import com.squareup.kotlinpoet.FunSpec
 import com.squareup.kotlinpoet.KModifier
 import com.squareup.kotlinpoet.ParameterSpec
+import com.squareup.kotlinpoet.PropertySpec
 import com.squareup.kotlinpoet.TypeSpec
 
 object NavGraphScopesCodegen : FilesGen {
 
     override fun generate(data: ModelData): List<GeneratorUnit> {
-       return data.navGraphs.map { generateNavScope(data.packageName, it) }
+        return data.navGraphs.map {
+            listOf(
+                generateNavScope(data.packageName, it),
+                generateNavScopeImplementation(data.packageName, it)
+            )
+        }.flatten()
     }
 
     private fun generateNavScope(packageName: String, graphData: NavGraphData): GeneratorUnit {
@@ -23,7 +32,7 @@ object NavGraphScopesCodegen : FilesGen {
             packageName, TypeSpec.interfaceBuilder("${graphData.graphName}Scope")
                 .apply {
                     graphData.routes.forEach {
-                        addFunction(it.toFunSpec())
+                        addFunction(it.toFunSpecBuilder().addModifiers(KModifier.ABSTRACT).build())
                     }
                 }
                 .build()
@@ -34,12 +43,48 @@ object NavGraphScopesCodegen : FilesGen {
         )
     }
 
-    private fun RouteData.toFunSpec() : FunSpec {
+
+    private fun generateNavScopeImplementation(
+        packageName: String,
+        graphData: NavGraphData
+    ): GeneratorUnit {
+
+        val property = PropertySpec.builder("navGraphBuilder", ClassNames.NavGraphBuilder)
+            .initializer("navGraphBuilder")
+            .addModifiers(KModifier.PRIVATE)
+
+        val typeBuilder = TypeSpec.classBuilder("${graphData.graphName}Builder")
+            .addSuperinterface(ClassName(packageName, "${graphData.graphName}Scope"))
+            .addModifiers(KModifier.INTERNAL)
+            .primaryConstructor(
+                FunSpec.constructorBuilder()
+                    .addParameter(
+                        name = "navGraphBuilder", ClassNames.NavGraphBuilder
+                    ).build())
+            .addProperty(property.build())
+
+
+
+        graphData.routes.forEach {
+            val funSpecBuilder = it.toFunSpecBuilder().addModifiers(KModifier.OVERRIDE)
+            funSpecBuilder.addCode(
+                CodeBlock.of(
+                    "navGraphBuilder.%T(%T, %T, content)",
+                    ClassNames.route,
+                    it.routeClassName,
+                    it.routeTransitionClass
+                )
+            )
+            typeBuilder.addFunction(funSpecBuilder.build())
+        }
+
+        return GeneratorUnit(FileSpec.get(packageName, typeBuilder.build()), graphData.sources)
+    }
+
+    private fun RouteData.toFunSpecBuilder(): FunSpec.Builder {
         val parameterSpec = ParameterSpec.builder("content", ComposableFunction).build()
         return FunSpec
             .builder(name.replaceFirstChar { it.lowercase() })
             .addParameter(parameterSpec)
-            .addModifiers(KModifier.ABSTRACT)
-            .build()
     }
 }
